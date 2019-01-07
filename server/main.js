b7b1d983402274fcbc6248df0ccb083561cf2fd7
Enmanet <Emanetproject@gmail.com>
2019-01-06 19:31:43 -0600

    removed individual modules. added helmet as global
    removed individual modules. added helmet as global security module includes:
    Content-Security-Policy
    Strict-Transport-Security
    Referrer-Policy
    X-Frame-Options
    X-XSS-Protection
    X-Content-Type-Options
    Feature-Policy

--- server/main.js
+++ server/main.js
@@ -9,19 +9,19 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const WebSocketServer = require('ws').Server;
 const helmet = require('helmet')
-const hsts = require('hsts')
-const redisClient = require('redis').createClient();
 const app = express();
-const crossdomain = require('helmet-crossdomain')
-const hpkp = require('hpkp')
-const csp = require('helmet-csp')
-const forceDomain = require('forcedomain');
-
-
-
 const log_file = require("fs").createWriteStream(__dirname + '/debug.log', {flags : 'w'});
 const log_stdout = process.stdout;
-const limiter = require('express-limiter')(app, redisClient);
+const forceDomain = require('forcedomain');
+
+//additional header security
+app.use(helmet())
+
+//Force Domain 
+app.use(forceDomain({
+  hostname: 'www.enmanet.com'
+}));
+
 console.log = function(d, userID) { 
     if (!g_constants.DEBUG_LOG)
         return;
@@ -33,105 +33,15 @@ console.log = function(d, userID) {
     require("./utils").log_user(userID, d);
 };
 
-
-app.use(forceDomain({
-  hostname: 'www.enmanet.com'
-}));
-
-app.use(hsts({
-  maxAge: 15552000  // 180 days in seconds
-}))
-app.use(helmet())
-app.disable('x-powered-by')
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 })); 
 
 
-app.use(csp({
-  // Specify directives as normal.
-  directives: {
-    defaultSrc: ["'self'", 'enmanet.com'],
-    scriptSrc: ["'self'", "'unsafe-inline'"],
-    sandbox: ['allow-forms', 'allow-scripts'],
-    reportUri: '/report-violation',
-    objectSrc: ["'none'"],
-    upgradeInsecureRequests: true,
-    workerSrc: false  // This is not set.
-  },
-
-  // This module will detect common mistakes in your directives and throw errors
-  // if it finds any. To disable this, enable "loose mode".
-  loose: false,
-
-  // Set to true if you only want browsers to report errors, not block them.
-  // You may also set this to a function(req, res) in order to decide dynamically
-  // whether to use reportOnly mode, e.g., to allow for a dynamic kill switch.
-  reportOnly: false,
-
-  // Set to true if you want to blindly set all headers: Content-Security-Policy,
-  // X-WebKit-CSP, and X-Content-Security-Policy.
-  setAllHeaders: false,
-
-  // Set to true if you want to disable CSP on Android where it can be buggy.
-  disableAndroid: false,
-
-  // Set to false if you want to completely disable any user-agent sniffing.
-  // This may make the headers less compatible but it will be much faster.
-  // This defaults to `true`.
-  browserSniff: true
-}))
-
-
-
-
-
-
-app.set('forceSSLOptions', {
-  enable301Redirects: true,
-  trustXFPHeader: false,
-  httpsPort: 443,
-  sslRequiredMessage: 'SSL Required.'
-});
-
-// Sets X-Permitted-Cross-Domain-Policies: none
-app.use(crossdomain())
-
-// You can use any of the following values:
-app.use(crossdomain({ permittedPolicies: 'master-only' }))
 
 
 //app.use(cookieParser());
-
-
-// Limit requests to 100 per hour per ip address.
-limiter({
-  lookup: ['connection.remoteAddress'],
-  total: 100,
-  expire: 1000 * 60 * 60
-})
-
-const ninetyDaysInSeconds = 7776000
-app.use(hpkp({
-  maxAge: ninetyDaysInSeconds,
-  sha256s: ['INSERT_VALID_HPKP_HASH', 'INSERT_VALID_KPKP_HASH'],
-  includeSubDomains: true,         // optional
-  reportUri: 'http://enmanet.com', // optional
-  reportOnly: false,               // optional
-
-  // Set the header based on a condition.
-  // This is optional.
-  setIf(req, res) {
-    return req.secure
-  }
-}))
-
-
-var compression = require('compression')
-
-// compress all responses
-app.use(compression())
 
 var httpServer = http.createServer(app);
 var httpsServer = https.createServer(g_constants.SSL_options, app);
@@ -210,6 +120,4 @@ require("./database").Init(() => {
     require('./reqHandler.js').handle(app, g_constants.WEB_SOCKETS);
 });
 //require("./modules/users/market").Init();
-
-
 
